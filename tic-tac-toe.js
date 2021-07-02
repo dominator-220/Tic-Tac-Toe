4d54ba46f56809bbe65ab14772a8695e34e19c4f
dominator-220 <anshujha271@gmail.com>
2021-07-02 17:33:27 +0530

    Added marvel icons for players along with different level of difficulty for ai and some normal styling

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,11 @@
-function player(name){
+function player(name,link){
     this.name=name;
+    this.link=link;
 }
 
 
+let lastmine="";
+let lastopponent="";
 
 let col1=[0,3,6];
 let col2=[1,4,7];
@@ -14,21 +17,38 @@ let diag1=[0,4,8];
 let diag2=[2,4,6];
 
 let configuration=[col1,col2,col3,row1,row2,row3,diag1,diag2];
+let twoplayer=document.querySelector("#twoplayer");
+let easy=document.querySelector("#easy");
+let medium=document.querySelector("#medium");
+let hard=document.querySelector("#hard");
+let invincible=document.querySelector("#invincible");
+let choice=document.querySelector("#choice");
+let character=document.querySelector("#character");
+let opponentcharacter=document.querySelector("#opponentcharacter");
+let wait1=document.querySelector("#wait1");
+let wait2=document.querySelector("#wait2");
+
+easy.addEventListener('click',Easy);
+twoplayer.addEventListener('click',Twoplayer);
+medium.addEventListener('click',Medium);
+hard.addEventListener('click',Hard);
+invincible.addEventListener('click',Invincible);
 
 let dict={};
 let moves=0;
-let person1=new player("ANS");
-let person2=new player("AKU");
-let ai=new player("AI");
+let person1=new player("","");
+let person2=new player("","");
+let ai=new player("","");
+let parent=document.querySelector("#parent");
 let gamearea=document.querySelector('#gamearea');
 let result=document.querySelector("#play");
 result.addEventListener('click',clear);
 
 
 for(let i=0;i<9;i++){
+    gamearea.style.display="none";
     let cell=document.createElement('div');
     cell.innerText="";
-    cell.addEventListener('click',add);
     gamearea.appendChild(cell);
 
     
@@ -39,12 +59,19 @@ function add(){
     console.log(result.style.display);
     if (result.style.display=="" | result.style.display=="none" ){
         let div=this;
+        let img=document.createElement('img');
         if (div.innerHTML==""){
             if (moves%2){
                 div.innerText=person2.name;
+                img.src=person2.link;
+                img.className="ingameimgchar";
+                div.appendChild(img);
             }
             else{
                 div.innerText=person1.name;
+                img.src=person1.link;
+                img.className="ingameimgchar";
+                div.appendChild(img);
             }
             moves++;
         }
@@ -82,15 +109,124 @@ function clear(){
     let children=gamearea.children;
     for(i=0;i<children.length;i++){
         children[i].innerText="";
-        children[i].removeEventListener('click',add);
-        children[i].addEventListener('click',invincibleai);
+        children[i].replaceWith(children[i].cloneNode(true));
+        console.log(children[i]);
+       
 
     }
     result.style.display="none";
+    gamearea.style.display="none";
+    choice.style.display="block";
+    character.style.display="block";
+    opponentcharacter.style.display="block";
+    person1.name="";
+    person1.link="";
+    person2.name="";
+    person2.link="";
+    ai.name="";
+    ai.link="";
+    if (lastmine!=""){
+        lastmine.style.outline="2px solid black";
+    }
+    if (lastopponent!=""){
+        lastopponent.style.outline="2px solid black";
+    }
+    lastmine="";
+    lastopponent="";
+    wait1.innerText="Player1";
+    wait2.innerText="Player2";
+    info.innerText="Choose Your Character";
     
 
 
+    
+
 }
+
+function Twoplayer(){
+    if (person1.name==""| person2.name==""){
+        return;
+    }
+    let children=gamearea.children;
+    for(i=0;i<children.length;i++){
+        children[i].addEventListener('click',add);
+
+    }
+    choice.style.display="none";
+    gamearea.style.display="grid";
+    character.style.display="none";
+    opponentcharacter.style.display="none";
+    info.innerText="";
+
+}
+function Easy(){
+    if (person1.name==""| person2.name==""){
+        return;
+    }
+    let children=gamearea.children;
+    for(i=0;i<children.length;i++){
+        children[i].addEventListener('click',easyai);
+
+    }
+    choice.style.display="none";
+    gamearea.style.display="grid";
+    character.style.display="none";
+    opponentcharacter.style.display="none";
+    info.innerText="";
+
+}
+function Medium(){
+    if (person1.name==""| person2.name==""){
+        return;
+    }
+    let children=gamearea.children;
+    for(i=0;i<children.length;i++){
+        children[i].addEventListener('click',mediumai);
+
+    }
+    choice.style.display="none";
+    gamearea.style.display="grid";
+    character.style.display="none";
+    opponentcharacter.style.display="none";
+    info.innerText="";
+
+}
+function Hard(){
+    if (person1.name==""| person2.name==""){
+        return;
+    }
+    let children=gamearea.children;
+    for(i=0;i<children.length;i++){
+        children[i].addEventListener('click',hardai);
+
+    }
+    choice.style.display="none";
+    gamearea.style.display="grid";
+    character.style.display="none";
+    opponentcharacter.style.display="none";
+    info.innerText="";
+    
+
+
+
+}
+function Invincible(){
+    if (person1.name==""| person2.name==""){
+        return;
+    }
+    let children=gamearea.children;
+    for(i=0;i<children.length;i++){
+        children[i].addEventListener('click',invincibleai);
+
+    } 
+    choice.style.display="none";
+    gamearea.style.display="grid";
+    character.style.display="none";
+    opponentcharacter.style.display="none";
+    info.innerText="";
+
+}
+
 
 function check(){
     for(states in configuration){
@@ -147,8 +283,12 @@ function easyai(){
     if (result.style.display=="" | result.style.display=="none" ){
        
         let div =this;
+        let img=document.createElement('img');
         if ((div.innerText=="")){
             div.innerText=person1.name;
+            img.src=person1.link;
+            img.className="ingameimgchar";
+            div.appendChild(img);
             moves++;
             if(check()){
                 alert(`Winner ${person1.name}`);
@@ -176,8 +316,12 @@ function easyai(){
                 }
             }
             let aiselection=arr[Math.floor(Math.random()*(arr.length))];
+            let img1=document.createElement('img');
             console.log(aiselection);
             child[aiselection].innerText=ai.name;
+            img1.src=ai.link;
+            img1.className="ingameimgchar";
+            child[aiselection].appendChild(img1);
             moves++;
             if(check()){
                 alert(`Winner ${ai.name}`);
@@ -207,8 +351,12 @@ function mediumai(){
        
         let div =this;
         if ((div.innerText=="")){
+            let img=document.createElement('img');
             
             div.innerText=person1.name;
+            img.src=person1.link;
+            img.className="ingameimgchar";
+            div.appendChild(img);
             moves++;
             if(check()){
                 alert(`Winner ${person1.name}`);
@@ -236,9 +384,15 @@ function mediumai(){
                 }
             }
             for(let i=0;i<arr.length;i++){
+                let img1=document.createElement('img');
                 child[arr[i]].innerText=ai.name;
+             
+
                
                 if(check()){
+                    img1.src=ai.link;
+                    img1.className="ingameimgchar";
+                    child[arr[i]].appendChild(img1);
                   
                     alert(`Winner ${ai.name}`);
                     result.style.display="block";
@@ -257,6 +411,11 @@ function mediumai(){
             let aiselection=arr[Math.floor(Math.random()*(arr.length))];
             console.log(aiselection);
             child[aiselection].innerText=ai.name;
+            let img1=document.createElement('img');
+            img1.src=ai.link;
+            img1.className="ingameimgchar";
+            child[aiselection].appendChild(img1)
+
             moves++;
             if(check()){
                 alert(`Winner ${ai.name}`);
@@ -286,8 +445,12 @@ function hardai(){
        
         let div =this;
         if ((div.innerText=="")){
-            
+         
+            let img=document.createElement('img');
             div.innerText=person1.name;
+            img.src=person1.link;
+            img.className="ingameimgchar";
+            div.appendChild(img);
             moves++;
             if(check()){
                 alert(`Winner ${person1.name}`);
@@ -318,6 +481,10 @@ function hardai(){
                 child[arr[i]].innerText=ai.name;
                
                 if(check()){
+                    let img1=document.createElement('img');
+                    img1.src=ai.link;
+                    img1.className="ingameimgchar";
+                    child[arr[i]].appendChild(img1);
                   
                     alert(`Winner ${ai.name}`);
                     result.style.display="block";
@@ -338,6 +505,10 @@ function hardai(){
                
                 if(check()){
                     child[arr[i]].innerText=ai.name;
+                    let img1=document.createElement('img');
+                    img1.src=ai.link;
+                    img1.className="ingameimgchar";
+                    child[arr[i]].appendChild(img1);
                   
                     
                     
@@ -355,6 +526,10 @@ function hardai(){
             let aiselection=arr[Math.floor(Math.random()*(arr.length))];
             console.log(aiselection);
             child[aiselection].innerText=ai.name;
+            let img1=document.createElement('img');
+            img1.src=ai.link;
+            img1.className="ingameimgchar";
+            child[aiselection].appendChild(img1)
             moves++;
             if(check()){
                 alert(`Winner ${ai.name}`);
@@ -390,7 +565,11 @@ function invincibleai(){
         let index=Array.from(gamearea.children).indexOf(div);
         if ((div.innerText=="")){
             
+            let img=document.createElement('img');
             div.innerText=person1.name;
+            img.src=person1.link;
+            img.className="ingameimgchar";
+            div.appendChild(img);
             moves++;
             if(check()){
                 alert(`Winner ${person1.name}`);
@@ -412,9 +591,17 @@ function invincibleai(){
                 console.log(dict[div]);
                 if (index==4){
                     gamearea.children[0].innerText=ai.name;
+                    let img1=document.createElement('img');
+                    img1.src=ai.link;
+                    img1.className="ingameimgchar";
+                    gamearea.children[0].appendChild(img1);
                 }
                 else{
                     gamearea.children[4].innerText=ai.name;
+                    let img1=document.createElement('img');
+                    img1.src=ai.link;
+                    img1.className="ingameimgchar";
+                    gamearea.children[4].appendChild(img1);
 
                 }
                 
@@ -472,6 +659,10 @@ function bestmove(){
     if(bestval!=-1000){
         console.log(pos,bestval);
         child[pos].innerText=ai.name;
+        let img1=document.createElement('img');
+        img1.src=ai.link;
+        img1.className="ingameimgchar";
+        child[pos].appendChild(img1);
     }
 }
 
@@ -598,4 +789,33 @@ function minimax(depth, isMax)
         
         return best;
     }
+}
+
+function myassign(div){
+    console.log(div.children[0].src);
+    person1.name=div.id;
+    person1.link=div.children[0].src;
+    
+
+    if (lastmine!=""){
+        lastmine.style.outline="2px solid black";
+    }
+    lastmine=div;
+    lastmine.style.outline="4px solid red";
+    wait1.innerText=div.id;
+
+    console.log(person1);
+}
+function opponentassign(div){
+    person2.name=div.id;
+    person2.link=div.children[0].src;
+    ai.name=div.id;
+    ai.link=div.children[0].src;
+    if (lastopponent!=""){
+        lastopponent.style.outline="2px solid black";
+    }
+    lastopponent=div;
+    lastopponent.style.outline="4px solid blue";
+    wait2.innerText=div.id;
+    console.log(person2,ai);
 }
